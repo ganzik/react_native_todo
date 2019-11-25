13e934773b2a8d272b43bbf859f400baddb781f5
kimkihogn <ganzik83@gmail.com>
2019-11-25 20:57:53 +0900

    [ADD] 기본 틀 잡기

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, StatusBar } from 'react-native';
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  Dimensions
+} from "react-native";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu'
-});
+const { height, width } = Dimensions.get("window");
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <StatusBar barStyle='light-content' />
+        <StatusBar barStyle="light-content" />
         <Text style={styles.title}>chacha main page</Text>
+        <View style={styles.card}>
+          <TextInput style={styles.input} placeholder={"New to do"} />
+        </View>
       </View>
     );
   }
@@ -21,24 +27,45 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    alignItems: "center",
+    backgroundColor: "#F23657"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
     margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
+    textAlign: "center",
+    color: "#333333",
     marginBottom: 5
   },
   title: {
-    color: 'orange',
+    color: "white",
     fontSize: 30,
-    fontWeight: '400',
-    marginTop: 50
+    fontWeight: "400",
+    marginTop: 50,
+    marginBottom: 30
+  },
+  card: {
+    backgroundColor: "white",
+    flex: 1,
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50, 50, 50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    })
   }
 });
